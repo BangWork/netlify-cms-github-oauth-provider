c317139ba8d80a7ea6e2dfb57f854c992c7c12a9
彭振聪 <pengzhencong@pengzhencongdeMacBook-Pro.local>
2020-08-12 22:14:01 +0800

    fix:kill掉原来掉进程

--- devops/scripts/remote.sh
+++ devops/scripts/remote.sh
@@ -36,27 +36,37 @@ ARGS="$@"
 parse_config
 echo "DATAPATH : $DATAPATH "
 echo "PKG : $PKG "
-echo "ID : $ID "
+echo "  : $ID "
 echo "SECRETY : $SECRETY "
 echo "ORIGIN : $ORIGIN "
 echo "PORT : $PORT "
 echo "NODEENV : $NODEENV "
 
 restart(){
-    echo "kp :$(lsof -t -i :3000)" 
-    kill -9 "$(lsof -t -i :3000)"
-    sleep 3
-    cd $DATAPATH 
-    rm -rf master 
-    mkdir master  
-    tar -xvf $PKG -C master 
-    rm -rf $PKG 
+    cd $DATAPATH
+    rm -rf master
+    mkdir master
+    tar -xvf $PKG -C master
+    #rm -rf $PKG
     cd master
-    export OAUTH_CLIENT_ID=$ID 
-    export OAUTH_CLIENT_SECRET=$SECRETY 
-    export ORIGIN=$ORIGIN 
-    export PORT=$PORT 
-    export NODE_ENV=$NODEENV 
-    nohup node index.js >> local_log 2>&1 & 
+    export OAUTH_CLIENT_ID=$ID
+    export OAUTH_CLIENT_SECRET=$SECRETY
+    export ORIGIN=$ORIGIN
+    export PORT=$PORT
+    export NODE_ENV=$NODEENV
 
+    echo "PID=$(lsof -t -i :$PORT)"
+    APPPID="$(lsof -t -i :$PORT)" && A=1
+
+    echo "xxx $APPPID"
+    if [[ $APPPID != "" ]]; then
+        echo "pid found: $APPPID"
+        kill -9 $APPPID
+        sleep 2
+    fi
+    echo "nohup node index.js >> local_log 2>&1 "
+    nohup node index.js >> local_log 2>&1 &
+    cd -
 }
+restart
+
