de8596a08f5ae5bddf224f0536a6bb2e5eb7891b
彭振聪 <pengzhencong@pengzhencongdeMacBook-Pro.local>
2020-08-12 21:36:35 +0800

    fix:kill掉原来掉进程

--- devops/scripts/deploy.sh
+++ devops/scripts/deploy.sh
@@ -17,31 +17,15 @@ pkg="netlify-cms-github-oauth-provider-$BRANCH_NAME-assets.tar.gz"
 # Upload web assets to dev online server
 # echo "rsync -vaz --progress '-e ssh -p '$ssh_port $pkg $user@$host:$data_path/"
 rsync -vaz --progress '-e ssh -p '$ssh_port $pkg $user@$host:$data_path/
-
+rsync -vaz --progress '-e ssh -p '$ssh_port devops/scripts/remote.sh $user@$host:$data_path/
 # Check exit status of previous command
+
 if [ $? != 0 ]; then
   exit 1
 fi
 
 echo 'Deploying...'
-ssh -p $ssh_port $user@$host <<'ENDSSH'
-#commands to run on remote host
-set -e
-echo "kp :$(lsof -t -i :3000)" 
-kill -9 "$(lsof -t -i :3000)" 
-cd $data_path 
-rm -rf master 
-mkdir master  
-tar -xvf $pkg -C master 
-rm -rf $pkg 
-cd master 
-export OAUTH_CLIENT_ID=$id 
-export OAUTH_CLIENT_SECRET=$secrety 
-export ORIGIN=$origin 
-export PORT=$port 
-export NODE_ENV=$node_env 
-nohup node index.js >> local_log 2>&1 & 
-ENDSSH
+ssh -p $ssh_port $user@$host /bin/bash $data_path/remote.sh --data_path=$DEV_ONLINE_DATA_PATH --id=$GITHUB_OAUTH_ID --secrety=$GITHUB_OAUTH_SECRET --node_env=$DEV_ONLINE_NODE_ENV --origin=$DEV_ONLINE_ORIGIN --port=$DEV_ONLINE_SERVER_PORT
 
 # Check exit status of previous command
 if [ $? != 0 ]; then
---
+++ devops/scripts/remote.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+set -e
+parse_config() {
+    for arg in ${ARGS}
+    do
+        case ${arg} in
+            --data_path=*)
+            DATAPATH="${arg#*=}"
+            ;;
+            --pkg=*)
+            PKG="${arg#*=}"
+            ;;
+            --id=*)
+            ID="${arg#*=}"
+            ;;
+            --secrety=*)
+            SECRETY="${arg#*=}"
+            ;;
+            --origin=*)
+            ORIGIN="${arg#*=}"
+            ;;
+            *)
+            --port=*)
+            PORT="${arg#*=}"
+            ;;
+            --node_env=*)
+            NODEENV="${arg#*=}"
+            ;;
+            # unknown option
+            usage
+            ;;
+        esac
+    done
+}
+
+ARGS="$@"
+parse_config
+echo "DATAPATH : $DATAPATH "
+echo "PKG : $PKG "
+echo "ID : $ID "
+echo "SECRETY : $SECRETY "
+echo "ORIGIN : $ORIGIN "
+echo "PORT : $PORT "
+echo "NODEENV : $NODEENV "
+
+restart(){
+    echo "kp :$(lsof -t -i :3000)" 
+    kill -9 "$(lsof -t -i :3000)" 
+    cd $DATAPATH 
+    rm -rf master 
+    mkdir master  
+    tar -xvf $PKG -C master 
+    rm -rf $PKG 
+    cd master 
+    export OAUTH_CLIENT_ID=$ID 
+    export OAUTH_CLIENT_SECRET=$SECRETY 
+    export ORIGIN=$ORIGIN 
+    export PORT=$PORT 
+    export NODE_ENV=$NODEENV 
+    nohup node index.js >> local_log 2>&1 & 
+}
